823f1c7542212640afefe2e25434813d87ba69f8
Adam <adar9394@gmail.com>
2016-11-02 15:53:05 -0500

    Update 2016-10-2 15:15:58

--- 1210.py
+++ 1210.py
@@ -1,31 +1,46 @@
 from subprocess import call
+call(["clear"])
+
+size = 0
 size = input("How big do you want the Board: ")
 world = [0 for x in range(int(size) + 1)]
-for x in range(0,int(size)):
-	world[x] = "_"
+def setup():
+	for x in range(0,int(size)):
+		world[x] = "_"
 
 def printBoard():
 	for x in range(0,int(size)):
 		print(world[x], end=' ')
 	print("\n")
-printBoard()
-location = 0
-player = 1
-while(world[int(size)-1] == "_"):
-	tile = input("Player 1, how many tile do you want to put down: ")
-	call(["clear"])
-	for x in range(0,int(tile)):
-		world[location] = "1"
-		location += 1
-	printBoard()
+
+
+def play():
+	location = 0
 	player = 1
-	if(world[int(size)-1] != "_"):
-		break
-	tile = input("Player 2, how many tile do you want to put down: ")
-	call(["clear"])
-	for x in range(0,int(tile)):
-		world[location] = "2"
-		location += 1
-	printBoard()
-	player = 2
-print("Player " + str(player)+ " wins")# 1210
+	while(world[int(size)-1] == "_"):
+		tile = input("Player 1, how many tile do you want to put down: ")
+		while (int(tile) != 1 and int(tile) != 2):
+			print("Please enter 1 or 2")
+			tile = input("Player 1, how many tile do you want to put down: ")
+		call(["clear"])
+		for x in range(0,int(tile)):
+			world[location] = "1"
+			location += 1
+		printBoard()
+		player = 1
+		if(world[int(size)-1] != "_"):
+			break
+		tile = input("Player 2, how many tile do you want to put down: ")
+		while (int(tile) != 1 and int(tile) != 2):
+			print("Please enter 1 or 2")
+			tile = input("Player 2, how many tile do you want to put down: ")
+		call(["clear"])
+		for x in range(0,int(tile)):
+			world[location] = "2"
+			location += 1
+		printBoard()
+		player = 2
+	print("Player " + str(player)+ " wins")
+setup()
+printBoard()
+play()
