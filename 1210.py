0d5572cb963d4d4640fd438e4b4c2e8609a11dc0
adar9394 <adam.riedel@valpo.edu>
2016-11-02 16:12:32 -0500

    Update 1210.py

--- 1210.py
+++ 1210.py
@@ -1,8 +1,12 @@
 from subprocess import call
 call(["clear"])
 
-size = 0
-size = input("How big do you want the Board: ")
+select = input("C = Choose \n"
+	+"N = Nah \n"+
+	"Choose: ")
+size = 10
+if (select == "C"):
+	size = input("How big do you want the Board: ")
 world = [0 for x in range(int(size) + 1)]
 def setup():
 	for x in range(0,int(size)):
@@ -14,7 +18,9 @@ def printBoard():
 	print("\n")
 
 
-def play():
+def play1DSet():
+	setup()
+	printBoard()
 	location = 0
 	player = 1
 	while(world[int(size)-1] == "_"):
@@ -41,6 +47,37 @@ def play():
 		printBoard()
 		player = 2
 	print("Player " + str(player)+ " wins")
-setup()
-printBoard()
-play()
+
+def play1DVari():
+	setup()
+	printBoard()
+	location = 0
+	player = 1
+	while(world[int(size)-1] == "_"):
+		tile = input("Player 1, how many tile do you want to put down: ")
+		while (int(tile) != 1 and int(tile) != 2):
+			print("Please enter 1 or 2")
+			tile = input("Player 1, how many tile do you want to put down: ")
+		call(["clear"])
+		for x in range(0,int(tile)):
+			world[location] = "1"
+			location += 1
+		printBoard()
+		player = 1
+		if(world[int(size)-1] != "_"):
+			break
+		tile = input("Player 2, how many tile do you want to put down: ")
+		while (int(tile) != 1 and int(tile) != 2):
+			print("Please enter 1 or 2")
+			tile = input("Player 2, how many tile do you want to put down: ")
+		call(["clear"])
+		for x in range(0,int(tile)):
+			world[location] = "2"
+			location += 1
+		printBoard()
+		player = 2
+	print("Player " + str(player)+ " wins")
+if (select == "C"):
+	play1DVari()
+elif (select == "N"):
+	play1DSet()
